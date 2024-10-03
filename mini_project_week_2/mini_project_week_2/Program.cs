7f3953f806bc388486ddd1b3649a85ec7ac73f1c
Erik Akerman <erikakerman@gmail.com>
2024-10-03 13:54:38 +0200

    basic list

--- mini_project_week_2/mini_project_week_2/Program.cs
+++ mini_project_week_2/mini_project_week_2/Program.cs
@@ -1,7 +1,30 @@
-﻿class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        System.Console.WriteLine("Hello World!");
+        List<string> itemList = new List<string>();
+
+        Console.WriteLine("Enter items to add to your list. Type 'exit' to finish.");
+
+        while (true)
+        {
+            Console.Write("Enter an item: ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "exit")
+                break;
+
+            itemList.Add(input);
+            Console.WriteLine($"'{input}' added. List now has {itemList.Count} item(s).");
+        }
+
+        Console.WriteLine("\nFinal list:");
+        foreach (string item in itemList)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
